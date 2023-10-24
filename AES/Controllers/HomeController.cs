16581c354542831b5485d44833e12ad365c05ea6
Raul <raul.rhf9316@gmail.com>
2023-10-23 22:51:59 -0300

    primeiro commit

---
+++ AES/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using AES.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AES.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(SecEntity secDetails)
+        {
+            return View(secDetails);
+        }
+
+        public IActionResult GenerateKey()
+        {
+            SecEntity detailsEntity = new SecEntity();
+
+            detailsEntity.AESKey = InfoSec.GenerateKey();
+
+            return View("Index", detailsEntity);
+        }
+
+        public IActionResult Encrypt(SecEntity details)
+        {
+            string IVKey = "";
+            details.CipherText = InfoSec.Encrypt(details.PlainText, details.AESKey, out IVKey);
+            details.AESIVKey = IVKey;
+
+            return RedirectToAction("Index", details);
+
+        }
+        public IActionResult Decrypt(SecEntity details)
+        {
+            string plainText = "";
+            details.CipherToPlainText = InfoSec.Decrypt(details.CipherText, details.AESKey, details.AESIVKey);
+
+
+
+            return RedirectToAction("Index", details);
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AES/InfoSec.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.Cryptography;
+
+namespace AES
+{
+    //code to generate the AES key
+    public static class InfoSec
+    {
+        public static string GenerateKey()
+        {
+            string keyBase64 = "";
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.GenerateKey();
+
+                keyBase64 = Convert.ToBase64String(aes.Key);
+            }
+
+            return keyBase64;
+        }
+
+        public static string Encrypt(string plainText, string key, out string ivKey)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Padding = PaddingMode.Zeros;
+                aes.Key = Convert.FromBase64String(key);
+                aes.GenerateIV();
+
+                ivKey = Convert.ToBase64String(aes.IV);
+
+                ICryptoTransform encryptor = aes.CreateEncryptor();
+
+                byte[] encryptedData;
+
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(plainText);
+                        }
+
+                        encryptedData = ms.ToArray();
+                    }
+                }
+                return Convert.ToBase64String(encryptedData);
+            }
+        }
+        public static string Decrypt(string cipher, string key, string ivKey)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Padding = PaddingMode.Zeros;
+                aes.Key = Convert.FromBase64String(key);
+                aes.IV = Convert.FromBase64String(ivKey);
+
+
+
+                ICryptoTransform decryptor = aes.CreateDecryptor();
+
+                string plainText = "";
+                byte[] cipherText = Convert.FromBase64String(cipher);
+
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader sr = new StreamReader(cs))
+                        {
+                            plainText = sr.ReadToEnd();
+                        }
+                    }
+                }
+                return plainText.ToString().TrimEnd(new char[] { '\0' });
+            }
+        }
+    }
+}
